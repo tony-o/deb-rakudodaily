7c7c0b29c133d90fbe98472030baa2efb98125ac
local <tony.odell@bestxperts.com>
2015-04-10 11:41:10 -0700

    bug fix in rebase

--- rebase.sh
+++ rebase.sh
@@ -3,7 +3,7 @@
 
 REBASE=$1
 if [ "$REBASE" == '' ]; then
-  REBASE="$PWD"
+  REBASE="$PWD/"
 fi
 
 grep -Irl '/home/tony/projects/rakudo/' ./* | grep -v './rebase.sh' | xargs -n 1 sed -i "s|/home/tony/projects/rakudo/|$REBASE|g" 
